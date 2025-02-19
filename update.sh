9ea2f80ebcfbfa47c587ff192a4cf2735c557749
genesha <genesha.irzan@gmail.com>
2025-02-20 01:53:17 +0700

    gip rebuild

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-cd /home/adminems/application
-git pull git main
+cd /home/adminems/project/ems_regis
+git pull git master
 npm i
 npm run build
-pm2 restart project_ems
+pm2 restart project_ems_regis

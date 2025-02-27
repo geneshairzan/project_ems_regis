206d323818b2256be830535e8d3460991a5c6850
genesha <genesha.irzan@gmail.com>
2025-02-27 21:45:09 +0700

    gip rebuild

--- application/src/component/gh/helper/useFetch.jsx
+++ application/src/component/gh/helper/useFetch.jsx
@@ -32,8 +32,10 @@ export const fetcher = async ({ saltY = accY, multipart = false, htoken, ...para
         accept: "application/json",
         "Accept-Language": "en-US,en;q=0.8",
 
-        "access-x": Md5.hashStr(accX + saltY),
-        "access-y": saltY,
+        // "access-x": Md5.hashStr(accX + saltY),
+        // "access-y": saltY,
+        "x-access-x": Md5.hashStr(accX + saltY),
+        "x-access-y": saltY,
         "Cache-Control": "no-cache",
         Pragma: "no-cache",
         Expires: "0",
--- application/src/component/middleware/server/index.jsx
+++ application/src/component/middleware/server/index.jsx
@@ -1,11 +1,19 @@
 import auth from "./auth";
 import header from "./header";
+import { Md5 } from "ts-md5";
 
 const middlewareChain = (handler) => async (req, res) => {
   // Compose middleware functions in the desired order
 
   if (!res.getHeader("Access-Control-Allow-Origin")) {
     res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+  let x = req.headers["x-access-x"];
+  let y = req.headers["x-access-y"];
+  let checkshum = Md5.hashStr("w}5opZ%3oIQ6Vq(PUsTL" + y);
+
+  if (checkshum != x) {
+    return res.status(401).json("no auth");
   }
 
   return header(auth(handler))(req, res);
